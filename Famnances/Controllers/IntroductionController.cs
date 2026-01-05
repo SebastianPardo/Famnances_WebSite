e434b9201bd3370c3efcb2273393963d21e34e98
Sebastian Pardo <js.pardo.j@gmail.com>
2026-01-05 16:27:06 -0500

    Welcome page added

---
+++ Famnances/Controllers/IntroductionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Famnances.Controllers
+{
+    public class IntroductionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: IntroductionController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IntroductionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
