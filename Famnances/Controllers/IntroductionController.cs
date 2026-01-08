d33400b5bdbfa4c473ff0b74316ca8b876fb5d38
Sebastian Pardo <js.pardo.j@gmail.com>
2026-01-08 16:35:30 -0500

    Introduction using languages

--- Famnances/Controllers/IntroductionController.cs
+++ Famnances/Controllers/IntroductionController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Famnances.DataCore.Entities;
+using Famnances.Helpers;
+using Famnances.Helpers.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Famnances.Controllers
 {
     public class IntroductionController : Controller
     {
-        public ActionResult Language()
+        IHttpHelper _httpHelper;
+        public IntroductionController(IHttpHelper httpHelper)
+        {
+            _httpHelper = httpHelper;
+        }
+        public async Task<ActionResult> Language()
         {
             return View();
         }
@@ -14,12 +22,57 @@ namespace Famnances.Controllers
             return View();
         }
 
-        public ActionResult PeriodSelector()
+        public async Task<ActionResult> PeriodSelector()
         {
-            return View();
+            var accountId = HttpContext.Session.GetString(Constants.ACCOUNT_ID);
+            var user = await _httpHelper.Get<User>($"{Constants.USER_URI}/{accountId}");
+
+            switch (Thread.CurrentThread.CurrentUICulture.ToString())
+            {
+                case "es-CO":
+                    user.Language = "ES";
+                    break;
+                case "en-CA":
+                    user.Language = "EN";
+                    break;
+                case "fr-CA":
+                    user.Language = "FR";
+                    break;
+                default:
+                    user.Language = "EN";
+                    break;
+            }
+
+            await _httpHelper.Put($"{Constants.USER_URI}/{accountId}", user);
+            List<Period> periods = await _httpHelper.Get<List<Period>>(Constants.PERIODS_URI);
+            return View(periods);
         }
-        public ActionResult Incomes()
+
+        public async Task<ActionResult> Incomes(string periodId)
         {
+            var accountId = HttpContext.Session.GetString(Constants.ACCOUNT_ID);
+            var user = await _httpHelper.Get<User>($"{Constants.USER_URI}/{accountId}");
+            user.PeriodId = (await _httpHelper.Get<Period>($"{Constants.PERIODS_URI}/{periodId}")).Id;
+            await _httpHelper.Put($"{Constants.USER_URI}/{accountId}", user);
+
+            List<Period> periods = await _httpHelper.Get<List<Period>>(Constants.PERIODS_URI);
+
+            switch (user.Language)
+            {
+                case "ES":
+                    ViewBag.Periods = new SelectList(await _httpHelper.Get<List<Period>>($"{Constants.PERIODS_URI}"), "Id", "NameES");
+                    break;
+                case "EN":
+                    ViewBag.Periods = new SelectList(await _httpHelper.Get<List<Period>>($"{Constants.PERIODS_URI}"), "Id", "NameEN");
+                    break;
+                case "FR":
+                    ViewBag.Periods = new SelectList(await _httpHelper.Get<List<Period>>($"{Constants.PERIODS_URI}"), "Id", "NameFR");
+                    break;
+                default:
+                    ViewBag.Periods = new SelectList(await _httpHelper.Get<List<Period>>($"{Constants.PERIODS_URI}"), "Id", "NameEN");
+                    break;
+            }
+
             return View();
         }
 
---
+++ Famnances/wwwroot/js/Introduction/PeriodSelector.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $(".period-card").on("click", function () {
+        $("#periodId").val(this.id);
+        $(".period-card").removeClass("active");
+        $(this).addClass("active");
+    });
+});
